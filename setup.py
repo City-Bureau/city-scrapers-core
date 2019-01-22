a22cfd4fb7345068cdf1b267b7c005a1b3376811
pjsier <pjsier@gmail.com>
2019-01-21 22:46:24 -0600

    Update legistar dep link

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ setup(
     install_requires=[
         "jsonschema>=3.0.0a5",
         "legistar",
-        "pytz>=2018.9,<2019.0",
-        "requests>=2.21,<3.0",
-        "scrapy>=1.5,<2.0",
+        "pytz",
+        "requests",
+        "scrapy",
     ],
-    tests_requires=["pytest>=4.1,<5.0", "flake8>=3.6,<4.0", "isort"],
-    extras_requires={"aws": ["boto3>=1.9"], "azure": ["azure-storage-blob>=1.4"]},
+    tests_requires=["flake8", "pytest", "isort"],
+    extras_requires={"aws": ["boto3"], "azure": ["azure-storage-blob"]},
     dependency_links=[
-        "git+https://github.com/opencivicdata/legistar.git@bf25cc94980f1608b4cc6766529f1155a273e049#egg=legistar"  # noqa
+        "https://github.com/opencivicdata/python-legistar-scraper/zipball/bf25cc94980f1608b4cc6766529f1155a273e049#egg=legistar"  # noqa
     ],
     python_requires=">=3.5,<4.0",
     classifiers=[
