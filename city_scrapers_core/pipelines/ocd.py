b477832edc22979dc15be645b31c0ddbdb5b2507
pjsier <pjsier@gmail.com>
2019-05-01 07:43:09 -0500

    Update OCD timezone handling

--- city_scrapers_core/pipelines/ocd.py
+++ city_scrapers_core/pipelines/ocd.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from uuid import uuid1
 
+import pytz
+
 from city_scrapers_core.decorators import ignore_processed
 
 
@@ -12,17 +14,18 @@ class OpenCivicDataPipeline:
 
     @ignore_processed
     def process_item(self, item, spider):
+        tz = pytz.timezone(spider.timezone)
         return {
             "_type": "event",
             "_id": item.get("_id") or "ocd-event/" + str(uuid1()),
-            "updated_at": datetime.now().isoformat()[:19],
+            "updated_at": tz.localize(datetime.now()).isoformat(timespec="seconds"),
             "name": item["title"],
             "description": item["description"],
             "classification": item["classification"],
             "status": item["status"],
             "all_day": item["all_day"],
-            "start_time": item["start"].isoformat()[:19],
-            "end_time": item["end"].isoformat()[:19],
+            "start_time": tz.localize(item["start"]).isoformat(timespec="seconds"),
+            "end_time": tz.localize(item["end"]).isoformat(timespec="seconds"),
             "timezone": spider.timezone,
             "location": self.create_location(item),
             "documents": [],
@@ -50,6 +53,6 @@ class OpenCivicDataPipeline:
 
     def create_location(self, item):
         loc_str = " ".join(
-            [item["location"]["name"], item["location"]["address"]]
+            [item["location"]["name"] or "", item["location"]["address"] or ""]
         ).strip()
         return {"url": "", "name": loc_str, "coordinates": None}
